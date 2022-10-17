d31e060faa5e5c70d5261deee89482a78f97fb85
ekulp <e.kulpin@mail.ru>
2022-10-17 14:58:26 +0300

    ready to review

--- homework/settings.py
+++ homework/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import about.apps
+import catalog.apps
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage.apps.HomepageConfig',
+    'catalog.apps.CatalogConfig',
+    'about.apps.AboutConfig'
 ]
 
 MIDDLEWARE = [
